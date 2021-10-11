906b78beb92c2430877030164626931f69d30472
Hugo Leonardo Gomes de Sousa <hlgomes.accenture@rd.com.br>
2021-10-11 09:02:26 -0300

    ajuste no repository

--- src/main/java/br/com/service/doit/repository/RoleRepository.java
+++ src/main/java/br/com/service/doit/repository/RoleRepository.java
@@ -1,11 +1,11 @@
 package br.com.service.doit.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.service.doit.model.Role;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findRoleByName(String name);
 }
--- src/main/java/br/com/service/doit/repository/UserRepository.java
+++ src/main/java/br/com/service/doit/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package br.com.service.doit.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.service.doit.model.Users;
 import br.com.service.doit.model.UserDto;
+import br.com.service.doit.model.Users;
 
 @Repository
-public interface UserRepository extends CrudRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
     Users findByUsername(String username);
 	void save(UserDto user);
 }
