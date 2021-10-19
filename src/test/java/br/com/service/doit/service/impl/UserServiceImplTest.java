3981db14a3e175ebc3a76a119bbf26b5bd313e26
Oliveira <jose.m.a.oliveira@accenture.com>
2021-10-19 10:45:22 -0300

    Test metodo findOne Patrick

--- src/test/java/br/com/service/doit/service/impl/UserServiceImplTest.java
+++ src/test/java/br/com/service/doit/service/impl/UserServiceImplTest.java
@@ -23,75 +23,80 @@ import br.com.service.doit.repository.UserRepository;
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class UserServiceImplTest {
-	
+
 	@Mock
 	private UserServiceImpl userServiceImpl;
-	
+
 	@Mock
 	private UserRepository userBanco;
-	
+
 	@Mock
 	private UserDetails detalhesUser;
-	
-	
+
 	@Test
 	public void testLoadByUserName() {
-		Users userTest = mock(Users.class);	
+		Users userTest = mock(Users.class);
 		when(userTest.getName()).thenReturn("user1");
 		userServiceImpl.loadUserByUsername(userTest.getName());
-		
+
 		assertEquals(userTest.getName(), "user1");
-		
+
 	}
-	
-	
+
 	@Test
-	public void testUserSave() {		
+	public void testUserSave() {
 		UserDto userMock = mock(UserDto.class);
-		
+
 		List<UserDto> list = new ArrayList<>();
-		
+
 		when(userMock.getName()).thenReturn("user1");
 		when(userMock.getPassword()).thenReturn("abc123");
-		
+
 		list.add(userMock);
 		System.out.println(list.get(0).getName());
 		userServiceImpl.save(list.get(0));
-		
+
 		assertEquals(list.get(0).getName(), "user1");
-		 
+
 	}
-	
+
+	@Test
+	public void findOne() {
+		Users users = new Users();
+		users.setUsername("teste");
+
+		when(userServiceImpl.findOne(users.getName())).thenReturn(users);
+		userServiceImpl.findOne("teste");
+
+		assertEquals(users.getUsername(), "teste");
+	}
+
 	@Test
 	public void testFindAll() {
 		List<Users> listAll = new ArrayList<>();
-		
+
 		Users userMock2 = mock(Users.class);
 		Users userMock3 = mock(Users.class);
 
 		listAll = userServiceImpl.findAll();
-		
+
 		when(userMock2.getName()).thenReturn("user2");
 		when(userMock2.getPassword()).thenReturn("abc123");
 		listAll.add(userMock2);
-		
+
 		when(userMock3.getName()).thenReturn("user3");
 		when(userMock3.getPassword()).thenReturn("abc123");
 		listAll.add(userMock3);
-		
+
 		System.out.println(listAll.size());
-		
+
 		assertEquals(listAll.size(), 2);
-		
-			
+
 	}
-	
-	
+
 	@Test
 	public void test3() {
 		assertEquals(null, 0, 0);
 	}
-	
-	
 
 }
